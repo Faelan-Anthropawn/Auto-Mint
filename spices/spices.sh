17ea86d7cae4b66dd4d37c37b5b7f5b1cd26e313
Faelan-Anthropawn <anthropawn@faelan.org>
2026-03-11 14:12:09 -0400

    Create spices.sh

---
+++ spices/spices.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+########################################
+# Replace Cinnamon Spices Directory
+########################################
+
+echo "Downloading and replacing Cinnamon spices directory..."
+
+SPICES_URL="https://github.com/Faelan-Anthropawn/Auto-Mint/tree/main/spices"
+
+TARGET_DIR="$HOME/.config/cinnamon/spices"
+
+rm -rf "$TARGET_DIR/*"
+
+mkdir -p "$TARGET_DIR"
+
+git clone --depth 1 --filter=blob:none --sparse https://github.com/Faelan-Anthropawn/Auto-Mint.git "$TMPDIR/Auto-Mint"
+
+cd "$TMPDIR/Auto-Mint"
+
+git sparse-checkout set spices
+
+cp -r "$TMPDIR/Auto-Mint/spices/." "$TARGET_DIR/"
+
+rm -rf "$TMPDIR/Auto-Mint"
+
+echo "Cinnamon spices directory has been successfully replaced with the latest files from GitHub."
